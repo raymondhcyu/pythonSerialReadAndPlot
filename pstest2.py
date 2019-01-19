596f5f2706f07954b5a55fa713f77706bf6aa8f5
Raymond <yuraymond96@gmail.com>
2019-01-19 00:07:55 +0000

    Created pyserial and plot tests

---
+++ pstest2.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import serial
+import platform
+
+print("Python version: " + platform.python_version())
+print("matplotlib version: " + mpl.__version__)
+
+fig, ax = plt.subplots()
+line, = ax.plot(np.random.rand(10))
+ax.set_ylim(0, 2000)
+xdata, ydata = [0]*100, [0]*100
+SerialIn = serial.Serial("COM14", 57600)
+
+def update(data):
+    line.set_ydata(data)
+    return line,
+
+def run(data):
+    global xdata, ydata
+    x,y = data
+    if (x == 0):
+        xdata = [0]*100
+        ydata = [0]*100
+    del xdata[0]
+    del ydata[0]
+    xdata.append(x)
+    ydata.append(y)
+    line.set_data(xdata, ydata)
+    return line,
+
+def data_gen():
+    x = 9
+    while True:
+        if (x >= 9):
+            x = 0
+        else:
+            x += 0.1
+        try:
+            inRaw = SerialIn.readline()
+            print(str(inRaw,'utf-8').strip('\r\n'))
+            inInt = int(inRaw)
+        except:
+            inInt = 0
+            print("Bad")
+
+        yield x, inInt
+
+ani = animation.FuncAnimation(fig, run, data_gen, interval=0, blit=True)
+
+plt.show()
---
+++ pythonSerialReadAndPlot.py
@@ -0,0 +1,37 @@
+import collections
+import serial
+import threading
+import time
+import numpy as np
+from itertools import islice
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+x = np.linspace(-100, 0, 1000) # make 1,000 points evenly distributed from -30 to 0
+y = collections.deque([0]*1000, maxlen=1000) # initialize the y to have 1000 points in it, or matplotlib will complain if x and y are not the same size
+
+
+def in_background():
+    serialport = serial.Serial("COM14", 57600, timeout=0.5)
+
+    while True:
+            command = serialport.readline()
+            console_output = str(command,'utf-8').strip('\r\n')
+            if console_output != "":
+                y.append(console_output)
+                print(console_output)
+    serialport.close()
+
+thread = threading.Thread(target = in_background)
+thread.start()
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+line, = ax1.plot(x, y)
+
+def animate(i):
+    line.set_ydata(y)
+
+ani = animation.FuncAnimation(fig, animate, interval=200)
+plt.ylim(0, 2000)
+plt.show()
