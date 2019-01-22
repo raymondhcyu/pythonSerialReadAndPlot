3525312d29a25780acf94f5554d650a1c35fad91
Raymond <yuraymond96@gmail.com>
2019-01-22 21:54:05 +0000

    SGCal Format Test and Creation

---
+++ SGCal.py
@@ -0,0 +1,98 @@
+# INFO
+# Read and receive data from serial port and print to console_output
+# Print serial only; for plotting see other file
+# User input COM and baud rate with exceptions if error
+#
+# QUICK START
+# 1) Flash microcontroller board with operational code
+# 2) Press and hold reset button microcontroller board to begin transmittion
+# 3) Transmitter: check for flashing red light on radio telemetry air module to confirm data being sent
+# 4) Receiver: check for solid green light on radio telemetry ground module to confirm pair with transmitter
+# 5) Operator: smile when you receive data
+#
+# Raymond Yu
+# 19 January 2019
+
+import os
+import sys
+import serial
+import time
+
+baudRateList = [110, 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400, 57600, 115200, 128000, 256000]
+
+def getUserInput():
+    print("*** Read serial data from COM port ***")
+    targetSerialPort = input("Enter COM port (e.g. COM14): ")
+    baudRate = int(input("Enter baud rate: ")) # convert input string to integer
+
+    # Remove all white spaces from COM import else error
+    targetSerialPort = targetSerialPort.replace(' ', '')
+
+    # Check if a correct baud rate entered
+    if baudRate not in baudRateList:
+        print("Error: Incorrect baud rate entered.")
+        sys.exit(1) # exit program
+    return targetSerialPort, baudRate
+
+# def parseData(inputData):
+#     warning1, warning2, warning3 = (' ', ' ', ' ')
+#     stringSplit = inputData.split(' ') # split input data by char
+#
+#     bat1 = stringSplit[1] # assign depending on location
+#     bat2 = stringSplit[3]
+#     aoa = stringSplit[5]
+#     ss = stringSplit[7]
+#
+#     try:
+#         if (int(bat1) < 500) or (int(bat2) < 500):
+#             warning1 = 'BATT WARNING'
+#         if int(aoa) > 100:
+#             warning2 = 'AOA WARNING'
+#         if int(ss) > 120:
+#             warning3 = 'SIDESLIP WARNING'
+#     except ValueError:
+#         pass
+#     return bat1, bat2, aoa, ss, warning1, warning2, warning3
+
+targetSerialPort, baudRate = getUserInput()
+
+try:
+    serialPort = serial.Serial(targetSerialPort, baudRate, \
+        bytesize = serial.EIGHTBITS, \
+        parity = serial.PARITY_NONE, \
+        stopbits = serial.STOPBITS_ONE, \
+        timeout = 1, \
+        xonxoff = False, \
+        rtscts = False)
+except serial.serialutil.SerialException: # serial port inaccessible error
+    print("Serial port cannot be found. Check COM port or if it is open in another program.")
+    sys.exit(1) # exit program
+
+while True:
+    try:
+        data = serialPort.readline()
+        # os.system("cls")
+        print(str(data,'utf-8').strip('\r\n')) # read data and remove carriage returns and newlines
+        # Bat_1, Bat_2, AoA, Sideslip, Warning1, Warning2, Warning3 = parseData(str(data,'utf-8').strip('\r\n'))
+        #
+        # print("Bat_1: " + Bat_1 + "V" \
+        #     + "\t" + "Bat_2: " + Bat_2 + "V" \
+        #     + "\t" + "AoA: " + AoA + "degs" \
+        #     + "\t" + "Sideslip: " + Sideslip + "degs"
+        #     + "\t" + Warning1 + "\t" + Warning2 + "\t" + Warning3)
+
+        # change individual bats to data array
+        # add future functionality to detect if no data being received
+        # print(data) # start, stop, carriage return, and quotes still attached
+    except IndexError:
+        pass
+    except UnicodeDecodeError: # check if data can be decoded
+        print("Unable to decode. Check baud rate.")
+        break
+    except KeyboardInterrupt: # means to stop program
+        print("Program stopped.")
+        break
+
+# Clean and close serial port for future use
+serialPort.flush()
+serialPort.close()
--- pstest3.py
+++ pstest3.py
@@ -25,38 +25,41 @@ import numpy as np
 # print(len(np.zeros(100)))
 
 
-# string = 'Bat_1: 639 Bat_2: 739 AoA: 63 Sideslip: 73 \nSG1: 904 SG2: 1004 SG3: 1104 SG4: 1204 SG5: 904 SG6: 1004 SG7: 1104 SG8: 1204'
-# print("Original string: " + string, type(string))
-#
-# stringSplit = string.split(' ')
-# # print(stringSplit)
+# string = 'Bat_1: 639 Bat_2: 739 AoA: 63 Sideslip: 73 \nSG1: 904.0 SG2: 1004.0 SG3: 1104.0 SG4: 1204.0 SG5: 904 SG6: 1004 SG7: 1104 SG8: 1204'
+string = '8.30 100.0 200.0 300.0 400.0 101.0 202.0 303.0 404.0'
+print("Original string: " + string, type(string))
+
+stringSplit = string.split(' ')
+print(stringSplit)
+
 # Bat_1 = stringSplit[1]
 # Bat_2 = stringSplit[3]
 # AoA = stringSplit[5]
 # Sideslip = stringSplit[7]
-#
-# SG = np.zeros(8)
-#
-# for i in range(len(SG)):
-#     SG[i] = stringSplit[(i + 9 + (i % 2) * 2)]
-#
-# SG[0] = stringSplit[9]
-# SG[1] = stringSplit[11]
-# SG[2] = stringSplit[13]
+Time = stringSplit[0]
+
+SG = np.zeros(8)
+
+for i in range(len(SG)):
+    SG[i] = stringSplit[(i + 1)]
+# print(SG)
+
+# SG[0] = stringSplit[1]
+# SG[1] = stringSplit[2]
+# SG[2] = stringSplit[3]
 # SG[3] = stringSplit[15]
 # SG[4] = stringSplit[17]
 # SG[5] = stringSplit[19]
 # SG[6] = stringSplit[21]
 # SG[7] = stringSplit[23]
-#
+
 # print("Bat_1: " + Bat_1 + "V")
 # print("Bat_2: " + Bat_2 + "V")
 # print("AoA: " + AoA + "degs")
 # print("Sideslip: " + Sideslip + "degs")
-#
-# # Change to for loop
-# for i in range(len(SG)):
-#     print("SG" + str(i) + ": " + str(SG[i]) + "V")
 
-for i in range(8):
-    print(i, i + 9 + (i % 2))
+for i in range(len(SG)):
+    print("SG" + str(i + 1) + ": " + str(SG[i]) + "V")
+
+# for i in range(8):
+#     print(i, i + 9 + (i % 2))
